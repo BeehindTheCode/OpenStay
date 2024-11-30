90a388c7d01da76d9c8dd3e0141287418db38fcf
FoggDev <51180821+FoggDev@users.noreply.github.com>
2024-11-29 22:04:16 -0600

    Implementing eslint, tailwind, nodemon, and global tsconfig

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
+
+const baseConfig = compat.extends('next/typescript')
+
+if (process.env.NEXT_LINTER) {
+  baseConfig.push(...compat.extends('next/core-web-vitals'))
+}
+
+const config = [
+  ...baseConfig,
+  {
+    rules: {
+      'no-console': 'warn'
+    }
+  }
+]
+
+export default config
